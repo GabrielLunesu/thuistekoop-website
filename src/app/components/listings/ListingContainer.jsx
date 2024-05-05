68fa8f90a7ee510d25f9266eef57040a9887994e
gabriel-lunesu <g.lunesu@outlook.com>
2024-05-05 12:34:13 +0200

    made the aanbod page dynamic

--- src/app/components/listings/ListingContainer.jsx
+++ src/app/components/listings/ListingContainer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Listing from './Listing';
 
-const ListingContainer = ({ listings }) => {
+const ListingContainer = ({ property }) => {
   return (
     <div className="flex flex-wrap justify-center items-center mx-auto max-w-7xl p-4">
-      {listings.map(listing => (
-        <Listing key={listing.id} listing={listing} />
+      {property.map(property => (
+        <Listing key={property.id} property={property} />
       ))}
     </div>
   );
